72af05a9ec3ae6646ca25e98578c6c3851879c49
Colin Smith <colin.foster.smith@icloud.com>
2018-12-30 14:03:50 -0500

    updated for py integration

--- auto-updater.sh
+++ auto-updater.sh
@@ -2,10 +2,10 @@
 
 
 
-read -p "What Version Do You Want To Download? " x
+value=$(<latest_version.txt)
 echo "Starting Process"
 rm -rf LMPServer
-wget "https://github.com/LunaMultiplayer/LunaMultiplayer/releases/download/""$x""/LunaMultiplayer-Release.zip"
+wget "https://github.com/LunaMultiplayer/LunaMultiplayer/releases/download/""$value""/LunaMultiplayer-Release.zip"
 unzip ~/LunaMultiplayer-Release.zip
 rm -rf ~/LMPClient
 rm -f ~/"LMP Readme.txt"
